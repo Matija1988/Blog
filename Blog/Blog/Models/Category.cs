529e4a5224ff81cd3c498e7e52a2f4d9726ef1b6
Matija Pavković <matijapavkovic74@gmail.com>
2024-04-16 13:18:39 +0200

    t

---
+++ Blog/Blog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class Category : Entity
+    {
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+    }
+}
---
+++ Blog/Blog/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public abstract class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+     
+    }
+}
