f558d38db0128a651efbea5f6c26a6322309b515
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-05-04 23:11:59 +0200

    Category POST

--- Blog/Blog/Controllers/CategoryController.cs
+++ Blog/Blog/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
@@ -13,8 +14,34 @@ namespace Blog.Controllers
         public IActionResult Index()
         {
             var categories = _context.Categories.ToList();  
+
+            if(categories == null || categories.Count < 0)
+            {
+                return NotFound("Entity list not found!!!");
+            } 
+
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category entity)
+        {
+            if(!ModelState.IsValid) 
+            { 
+              return StatusCode(StatusCodes.Status400BadRequest, "Model state is not valid!!!");
+            }
+
+            _context.Categories.Add(entity);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
 
