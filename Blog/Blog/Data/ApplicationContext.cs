9f2849058941f32c03d877b35f06ca42aec48138
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-05-04 20:51:16 +0200

    DBContext, migracije

---
+++ Blog/Blog/Data/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+        { 
+            
+        
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- Blog/Blog/Models/Category.cs
+++ Blog/Blog/Models/Category.cs
@@ -10,5 +10,6 @@ namespace Blog.Models
 
         public string CategoryDescription { get; set; }
 
+
     }
 }
