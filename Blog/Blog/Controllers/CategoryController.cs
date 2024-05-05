e40547d3779fd1e0d59af771afc68340b0dd2840
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-05-05 11:21:55 +0200

    Category POST automatizacija DisplayOrder (prazan unos rezultira postavljanjem broja na kraj liste, unos vec postojeceg broja iterira kroz listu postojecih unosa i kreira unos na kraju liste, moguce je preskociti red)

--- Blog/Blog/Controllers/CategoryController.cs
+++ Blog/Blog/Controllers/CategoryController.cs
@@ -13,12 +13,12 @@ namespace Blog.Controllers
         }
         public IActionResult Index()
         {
-            var categories = _context.Categories.ToList();  
+            var categories = _context.Categories.ToList();
 
-            if(categories == null || categories.Count < 0)
+            if (categories == null || categories.Count < 0)
             {
                 return NotFound("Entity list not found!!!");
-            } 
+            }
 
             return View(categories);
         }
@@ -31,14 +31,41 @@ namespace Blog.Controllers
         [HttpPost]
         public IActionResult Create(Category entity)
         {
-            if(!ModelState.IsValid) 
+            var Categories = _context.Categories.ToList();
+            int Order = 1;
+
+            List<int> OrderList = Categories.Select(s => s.DisplayOrder).Distinct().ToList();
+
+            if(OrderList.Any(o => o.Equals(entity.DisplayOrder))) 
             { 
-              return StatusCode(StatusCodes.Status400BadRequest, "Model state is not valid!!!");
+                
+                OrderList.ForEach(o =>
+                {
+                    if (o.Equals(Order))
+                    {
+                        Order++;
+                    }
+                });
+
+                entity.DisplayOrder = Order;
+
             }
 
-            _context.Categories.Add(entity);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
+            if (entity.DisplayOrder == 0)
+            {
+                entity.DisplayOrder = Order + Categories.Count;
+            }
+
+            if (entity.Name != null && entity.CategoryDescription != null)
+            {
+                _context.Categories.Add(entity);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
         }
 
 
--- Blog/Blog/Models/Category.cs
+++ Blog/Blog/Models/Category.cs
@@ -5,9 +5,10 @@ namespace Blog.Models
     public class Category : Entity
     {
         [Required]
+        [StringLength(50, ErrorMessage = "Maximum allowed number of characters: 50")]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
-
+        [StringLength(1000, ErrorMessage ="Maximum allowed number of characters: 1000")]
         public string CategoryDescription { get; set; }
 
 
