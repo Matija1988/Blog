f5c907d794648ef7fd548cd5bc48de3d5135c3fb
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-05-06 16:54:05 +0200

    Update / Delete for category

--- Blog/Blog/Controllers/CategoryController.cs
+++ Blog/Blog/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
-﻿using Blog.Data;
+﻿using Azure.Core;
+using Blog.Data;
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
@@ -32,53 +34,27 @@ namespace Blog.Controllers
         public IActionResult Create(Category entity)
         {
             var Categories = _context.Categories.ToList();
+            
             int Order = 1;
-
+            
             List<int> OrderList = Categories.Select(s => s.DisplayOrder).ToList();
 
-            if (!OrderList.Any(o => o.Equals(entity.DisplayOrder)) && entity.DisplayOrder != 0)
+            if (!OrderList.Any(o => o.Equals(entity.DisplayOrder)) && entity.DisplayOrder > 0)
             {
                 entity.DisplayOrder = entity.DisplayOrder;
             }
             else
             {
-
                 foreach (var category in Categories)
                 {
-
-                    if (OrderList.Any(o => o.Equals(entity.DisplayOrder)) || entity.DisplayOrder == 0)
+                    if (OrderList.Any(o => o.Equals(entity.DisplayOrder)) || entity.DisplayOrder <= 0)
                     {
                         Order++;
                     }
                     entity.DisplayOrder = Order + 1;
                 }
             }
-            //if(OrderList.Any(o => o.Equals(entity.DisplayOrder)) || entity.DisplayOrder == 0)
-            //{
-            //    int Last = OrderList.Last();
-            //    OrderList.ForEach(o =>
-            //    {
-            //        if (o.Equals(entity.DisplayOrder))
-            //        {         
-            //            Order++;
-            //        }
-
-            //        if(o.Equals(Order))
-            //        {
-            //            Order++;
-            //        }
-
-            //        if(Last == Order)
-            //        {
-            //            Order++;
-            //        }
-
-            //    });
-
-            //    entity.DisplayOrder = Order;
-
-            //}
-
+            
             if (entity.Name != null && entity.CategoryDescription != null)
             {
                 _context.Categories.Add(entity);
@@ -91,7 +67,79 @@ namespace Blog.Controllers
 
         }
 
+        public IActionResult Edit(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
 
+            var category = _context.Categories.Find(id);
+
+            if(category == null)
+            {
+                return NotFound();  
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category entity)
+        {
+            var Categories = _context.Categories.ToList();
+
+            int Order = 1;
+
+            List<int> OrderList = Categories.Select(s => s.DisplayOrder).ToList();
+
+            if (!OrderList.Any(o => o.Equals(entity.DisplayOrder)) && entity.DisplayOrder > 0)
+            {
+                entity.DisplayOrder = entity.DisplayOrder;
+            }
+            else
+            {
+                foreach (var category in Categories)
+                {
+                    if (OrderList.Any(o => o.Equals(entity.DisplayOrder)) || entity.DisplayOrder <= 0)
+                    {
+                        Order++;
+                    }
+                    entity.DisplayOrder = Order + 1;
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(entity);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var entity = _context.Categories.Find(id)
+                ?? throw new Exception("Entity with id" + id + " not found in database!");
+
+
+            return View(entity);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeleteCategory(Category entity)
+        {
+            
+            _context.Categories.Remove(entity);
+            _context.SaveChanges();
+             return RedirectToAction("Index");           
+        }
     }
 }
 
